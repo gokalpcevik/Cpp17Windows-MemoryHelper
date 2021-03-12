0db5ca39f3f1749ea760d627ba583063a28a4964
Gökalp Çevik <again4life@hotmail.com>
2021-03-12 15:07:20 +0300

    Add files via upload

---
+++ MemoryHelper.cpp
@@ -0,0 +1,133 @@
+﻿#include "MemoryHelper.h"
+
+
+std::optional<uint32_t> MemoryHelper::GetProcessId(std::wstring_view InProcessName)
+{
+	PROCESSENTRY32W PE32;
+	PE32.dwSize = sizeof(PROCESSENTRY32W);
+	void* hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
+	if(hSnapshot == INVALID_HANDLE_VALUE)
+		return std::nullopt;
+	Process32First(hSnapshot,&PE32);
+	do
+    {
+        if(PE32.szExeFile == InProcessName)
+            return static_cast<uint32_t>(PE32.th32ProcessID);
+        
+    } while(Process32Next(hSnapshot,&PE32));
+	CloseHandle(hSnapshot);
+	return std::nullopt;
+}
+
+std::optional<PROCESSENTRY32W> MemoryHelper::GetPE32(std::wstring_view InProcessName)
+{
+	PROCESSENTRY32W PE32;
+	PE32.dwSize = sizeof(PROCESSENTRY32W);
+	void* hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
+	if(hSnapshot == INVALID_HANDLE_VALUE)
+		return std::nullopt;
+	Process32First(hSnapshot,&PE32);
+	do
+    {
+        if(PE32.szExeFile == InProcessName)
+            return PE32;
+        
+    } while(Process32Next(hSnapshot,&PE32));
+	CloseHandle(hSnapshot);
+	return std::nullopt;
+}
+
+std::optional<uintptr_t> MemoryHelper::GetModuleAddress(std::wstring_view InModuleName,const uint32_t ProcessId)
+{
+	MODULEENTRY32W ME32;
+    ME32.dwSize = sizeof(MODULEENTRY32);
+    
+    void* hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE,ProcessId);
+    if(hSnapshot == INVALID_HANDLE_VALUE)
+        return -1;
+
+    Module32Next(hSnapshot,&ME32);
+    do
+    {
+        if(InModuleName == ME32.szModule)
+        {
+            return reinterpret_cast<uintptr_t>(ME32.modBaseAddr);
+        }
+    } while(Module32Next(hSnapshot,&ME32));
+
+	return std::nullopt;
+}
+
+std::optional<MODULEENTRY32W> MemoryHelper::GetME32(std::wstring_view InModuleName, const uint32_t ProcessId)
+{
+	MODULEENTRY32W ME32;
+    ME32.dwSize = sizeof(MODULEENTRY32);
+    
+    void* hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE,ProcessId);
+    if(hSnapshot == INVALID_HANDLE_VALUE)
+        return std::nullopt;
+
+    Module32Next(hSnapshot,&ME32);
+    do
+    {
+        if(InModuleName == ME32.szModule)
+        {
+            return ME32;
+        }
+    } while(Module32Next(hSnapshot,&ME32));
+
+	return std::nullopt;
+}
+
+bool MemoryHelper::EnableDebugPrivileges(void* ProcessHandle) noexcept
+{
+	void* hToken;
+    LUID luid;
+    TOKEN_PRIVILEGES tkp;
+	
+    const bool TokenSuccess = OpenProcessToken(ProcessHandle, TOKEN_ADJUST_PRIVILEGES, &hToken);
+    const bool LUPVSuccess = LookupPrivilegeValue(nullptr, SE_DEBUG_NAME, &luid);
+
+    tkp.PrivilegeCount = 1;
+    tkp.Privileges[0].Luid = luid;
+    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    const bool AdjustTokenValueSuccess = AdjustTokenPrivileges(hToken, false, &tkp, sizeof(tkp), nullptr, nullptr);
+    CloseHandle(ProcessHandle);
+    CloseHandle(hToken);
+	if(GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+		return false;
+	return TokenSuccess && LUPVSuccess && AdjustTokenValueSuccess;
+}
+
+std::optional<void*> MemoryHelper::OpenProcess(const uint32_t DesiredAccess, const bool InheritHandle, std::wstring_view InProcessName) noexcept
+{
+	PROCESSENTRY32W PE32;
+	PE32.dwSize = sizeof(PROCESSENTRY32W);
+	void* hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
+	if(hSnapshot == INVALID_HANDLE_VALUE)
+		return std::nullopt;
+	Process32First(hSnapshot,&PE32);
+	do
+    {
+        if(PE32.szExeFile == InProcessName)
+        {
+        	void* hProcess = ::OpenProcess(DesiredAccess,InheritHandle,PE32.th32ProcessID);
+        	if(hProcess == nullptr)
+				return std::nullopt;
+			return hProcess;
+        }
+        
+    } while(Process32Next(hSnapshot,&PE32));
+	CloseHandle(hSnapshot);
+	return std::nullopt;
+}
+
+std::optional<void*> MemoryHelper::OpenProcess(const uint32_t DesiredAccess, const bool InheritHandle,
+	const uint32_t InProcessId) noexcept
+{
+	void* hProcess = ::OpenProcess(DesiredAccess,InheritHandle,InProcessId);
+	if(hProcess == nullptr)
+		return std::nullopt;
+	return hProcess;
+}
+
---
+++ MemoryHelper.h
@@ -0,0 +1,97 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+#define NOMINMAX
+#define STRICT
+
+#include <Windows.h>
+#include <optional>
+#include <sstream>
+#include <memory>
+#include <TlHelp32.h>
+
+
+class MemoryHelper final 
+{
+public:
+	MemoryHelper(const MemoryHelper&) = delete;
+	MemoryHelper& operator =(const MemoryHelper&) = delete;
+	MemoryHelper() = default;
+
+	[[nodiscard]] static std::optional<uint32_t> GetProcessId(std::wstring_view InProcessName);
+
+	[[nodiscard]] static std::optional<PROCESSENTRY32W> GetPE32(std::wstring_view InProcessName);
+
+	[[nodiscard]] static std::optional<uintptr_t> GetModuleAddress(std::wstring_view InModuleName,const uint32_t ProcessId);
+
+	[[nodiscard]] static std::optional<MODULEENTRY32W> GetME32(std::wstring_view InModuleName,const uint32_t ProcessId);
+
+	template<typename T>
+	[[nodiscard]] static std::optional<T> Read(void* ProcessHandle, const uintptr_t Address) noexcept;
+
+	template<typename T>
+	static bool Write(void* ProcessHandle, const uintptr_t Address, const T& InValue) noexcept;
+
+	static bool EnableDebugPrivileges(void* ProcessHandle) noexcept;
+	
+	//To open a handle to another local process and obtain full access rights, you must enable the SeDebugPrivilege privilege.
+	[[nodiscard]]static std::optional<void*> OpenProcess(const uint32_t DesiredAccess, const bool InheritHandle, std::wstring_view InProcessName) noexcept;
+
+	//To open a handle to another local process and obtain full access rights, you must enable the SeDebugPrivilege privilege.
+	[[nodiscard]]static std::optional<void*> OpenProcess(const uint32_t DesiredAccess, const bool InheritHandle, const uint32_t InProcessId) noexcept;
+};
+
+template <typename T>
+[[nodiscard]] std::optional<T> MemoryHelper::Read(void* ProcessHandle, const uintptr_t Address) noexcept
+{
+	T Buffer;
+	DWORD PrevProtect;
+	VirtualProtectEx(ProcessHandle,reinterpret_cast<void*>(Address),sizeof(T),PAGE_EXECUTE_READ,&PrevProtect);
+	bool Success = ::ReadProcessMemory(ProcessHandle, reinterpret_cast<const void*>(Address),std::addressof(Buffer),sizeof(T),nullptr);
+	VirtualProtectEx(ProcessHandle,reinterpret_cast<void*>(Address),sizeof(T),PrevProtect,nullptr);
+	if(Success)
+		return std::optional<T>(std::move(Buffer));
+	return std::nullopt;
+}
+
+template <typename T>
+bool MemoryHelper::Write(void* ProcessHandle,const uintptr_t Address, const T& InValue) noexcept
+{
+	DWORD PrevProtect;
+	VirtualProtectEx(ProcessHandle,reinterpret_cast<void*>(Address),sizeof(T),PAGE_EXECUTE_READWRITE,&PrevProtect);
+	bool Success = ::WriteProcessMemory(ProcessHandle,reinterpret_cast<void*>(Address),std::addressof(InValue),sizeof(T),nullptr);
+	VirtualProtectEx(ProcessHandle,reinterpret_cast<void*>(Address),sizeof(T),PrevProtect,nullptr);
+	return Success;
+}
